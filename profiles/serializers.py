64c42d7346376fe2507f3b06e07f8a045963df74
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-03-08 09:53:00 +0000

    added serializer and added to views.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,7 +5,9 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
-    fields = [
-        'id', 'owner', 'created_at', 'updated_at', 'name',
-        'content', 'image',
-    ]
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image'
+        ]
